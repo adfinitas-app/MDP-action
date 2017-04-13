79cce8dbc40805d980bcd6d1069b20085a409ef6
Sebastien Bruere <sebastien.bruere@epitech.eu>
2017-04-13 10:19:04 +0200

    retour LP Passerelles d'avenir

--- passerelles-davenir/assets/js/app.js
+++ passerelles-davenir/assets/js/app.js
@@ -7,13 +7,38 @@ $(document).ready( function() {
     $(this).next('.hidden').slideDown( "slow", function() {
     });
   });
+  $('.down.un').click( function() {
+    $('.hidden.un').slideDown( "slow", function() {
+    }).prev('.arrow.down').css('visibility','hidden');
+  });
+  $('.down.deux').click( function() {
+    $('.hidden.deux').slideDown( "slow", function() {
+    }).prev('.arrow.down').css('visibility','hidden');
+  });
+  $('.down.trois').click( function() {
+    $('.hidden.trois').slideDown( "slow", function() {
+    }).prev('.arrow.down').css('visibility','hidden');
+  });
   $('.arrow.up').click( function() {
     $(this).parent().slideUp( "slow", function() {
       $(this).prev().css('visibility','visible');
     });
+  });
+  $('#bt-scroll-header').click( function() {
+    scrollTo($('.row.video'));
   });
 
 });
 $(window).resize( function() {
 
 });
+
+function    scrollTo(next){
+  if ($(next).length != 0)
+  {
+    $('html, body').stop().animate({
+      scrollTop: $(next).offset().top + 1
+    }, 700, 'swing');
+    return false;
+  }
+};
