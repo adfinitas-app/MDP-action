6c04955673ba4621c45d71127ab39320412a2a8a
Gonzalez Grégoire <gregoire.gonzalez-de-linares@epitech.eu>
2018-10-24 10:16:24 +0200

    Added: sticky button on small + some animations for donations box

--- duvet/js/app.js
+++ duvet/js/app.js
@@ -71,6 +71,22 @@ $(document).ready(setPage());
 
 window.onresize = setTopBar;
 
+window.onscroll = setStickyButton;
+
+function setStickyButton() {
+    let button = document.getElementById("smallStickyButton");
+
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        button.style.display = "block";
+        button.classList.add("is-stuck");
+        button.classList.remove("is-anchored");
+    } else {
+        button.style.display = "none";
+        button.classList.remove("is-stuck");
+        button.classList.add("is-anchored");
+    }
+}
+
 var img = document.getElementById("photoDuvet");
 
 img.addEventListener('onmouseover', unzipDuvet, {passive: false});
