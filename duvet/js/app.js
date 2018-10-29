5e528b1305bc6a878b3a7bb56a713bd5039fc036
Arnaud Masselin <web@adfinitas.fr>
2018-10-29 08:44:05 +0000

    Updated 'duvet/js/app.js' via CloudCannon

--- duvet/js/app.js
+++ duvet/js/app.js
@@ -122,8 +122,10 @@ function getNbrDuvetBought() {
         dataType: "text",
         success: function(response) {
             obj = JSON.parse(response);
-            for (let i = 0; i < obj.products.length; i++)
+            for (let i = 0; i < obj.products.length; i++) {
+                console.log(res);
                 res += parseInt(obj.products[i]);
+            }
             res = Math.trunc(res / 3000);
             progressBar.stop(0);
         }
