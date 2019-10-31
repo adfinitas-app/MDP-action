73c4d43007474f1a0254b4f506408ac67c737d41
Arnaud Masselin <web@adfinitas.fr>
2019-10-31 02:38:31 +0000

    Updated 'lespetitsmatinsdhiver/js/app.js' via CloudCannon

--- lespetitsmatinsdhiver/js/app.js
+++ lespetitsmatinsdhiver/js/app.js
@@ -1,2 +1,57 @@
 $(document).foundation();
 
+$("#logo").click(function () {
+    woopra.track('interaction', {category:"LOGO_FA19",action:"clic",url:document.location.href,title: document.title});
+});
+
+$("#logo-small").click(function () {
+    woopra.track('interaction', {category:"LOGO_FA19",action:"clic",url:document.location.href,title: document.title});
+});
+
+$("#cta").click(function () {
+    woopra.track('interaction', {category:"DON-CTA_FA19",action:"clic",url:document.location.href,title: document.title});
+});
+
+$("#cta-small").click(function () {
+    woopra.track('interaction', {category:"DON-CTA_FA19",action:"clic",url:document.location.href,title: document.title});
+});
+
+$("#eq1").click(function () {
+    woopra.track('interaction', {category:"DON-CTA-55_FA19",action:"clic",url:document.location.href,title: document.title});
+});
+
+$("#eq2").click(function () {
+    woopra.track('interaction', {category:"DON-CTA-100_FA19",action:"clic",url:document.location.href,title: document.title});
+});
+
+$("#eq3").click(function () {
+    woopra.track('interaction', {category:"DON-CTA-165_FA19",action:"clic",url:document.location.href,title: document.title});
+});
+
+$("#autre").click(function () {
+    woopra.track('interaction', {category:"DON-CTA-AUTRE_FA19",action:"clic",url:document.location.href,title: document.title});
+});
+
+var interval;
+
+function increase_number() {
+    var number = document.getElementById("number");
+    var value = number.innerHTML;
+
+    value = value.slice(0, 3) + value.slice(4);
+    value = parseInt(value) + 75;
+
+    if (value >= 150000) {
+        value = 150000;
+        clearInterval(interval);
+    }
+    value = value.toString();
+    while (value.length < 6) {
+        value = "0" + value;
+    }
+    number.innerHTML = value.slice(0, 3) + " " + value.slice(3);
+}
+
+$(function () {
+    interval = setInterval("increase_number()", 1);
+});
