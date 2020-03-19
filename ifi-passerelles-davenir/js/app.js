7fea8cd9a3831be0cbfd9d26ecb4d2b6e0fdd347
cauettheo <theo.cauet@epitech.eu>
2020-03-19 17:46:27 +0100

    [*] starting modifs

--- ifi-passerelles-davenir/js/app.js
+++ ifi-passerelles-davenir/js/app.js
@@ -1,19 +1,5 @@
 $(document).foundation();
-$(document).ready( function() {
-    var sliderlydia = document.getElementById("slider");
-    sliderlydia.classList.add("notDisplay");
-    if(window.location.href.indexOf("#Lydia") > -1) showSlider("lydia");
-    if(window.location.href.indexOf("#Gerard") > -1) showSlider("gerard");
-    if(window.location.href.indexOf("#Mohammad") > -1) showSlider("mohammad");
-    if(window.location.href.indexOf("#calendrier") > -1) showCalendar();
-    if(window.location.href.indexOf("#calculatrice") > -1) {
-        var elem = new Foundation.Reveal($('#calculatriceIFI'));
-        elem.open();
-        showCalcIFI();
-        bool = true;
-    }
-});
-var bool = false;
+
 var minus = [];
 var plus = [];
 var text = [];
